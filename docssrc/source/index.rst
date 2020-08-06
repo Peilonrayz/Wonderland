dc5c4d79b47cbf798057a1a9d7ee3e824ce17c36
Peilonrayz <peilonrayz@gmail.com>
2020-08-06 17:01:44 +0100

    Initial version

---
+++ docssrc/source/index.rst
@@ -0,0 +1,15 @@
+Peilonrayz' Guide to Wonderland
+===============================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   rules
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ noxfile.py
@@ -0,0 +1,36 @@
+import shutil
+
+import nox
+
+
+def docs_command(builder):
+    return [
+        "sphinx-build",
+        "-b",
+        builder,
+        "docssrc/source",
+        "docssrc/build/_build/{}".format(builder),
+    ]
+
+
+@nox.session
+def docs(session):
+    session.notify("docs_test")
+    session.notify("docs_build")
+
+
+@nox.session(python="3.8")
+def docs_test(session):
+    session.install("sphinx", "sphinx_rtd_theme", "sphinx-autodoc-typehints")
+    shutil.rmtree("docssrc/build/", ignore_errors=True)
+    session.run(*docs_command("doctest"))
+    session.run(*docs_command("linkcheck"))
+    session.run(*docs_command("html"))
+    shutil.rmtree("docssrc/build/", ignore_errors=True)
+
+
+@nox.session(python="3.8")
+def docs_build(session):
+    session.install("sphinx", "sphinx_rtd_theme", "sphinx-autodoc-typehints")
+    shutil.rmtree("docs/", ignore_errors=True)
+    session.run("sphinx-build", "-b", "html", "docssrc/source", "docs", "-a")
